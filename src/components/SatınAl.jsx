641ab13537df6abb2870bd62064f3dde4b7be920
AyyildizRamazan1 <ramaz01329@gmail.com>
2023-12-19 15:24:02 +0300

    edit purchase page

--- src/components/SatınAl.jsx
+++ src/components/SatınAl.jsx
@@ -1,5 +1,24 @@
 import React from "react";
-import kartResim from "../images/kart-resim.jpg"
+
+import Navbar from "./Navbar";
+import { GiBuyCard } from "react-icons/gi";
+import { BiCartAdd } from "react-icons/bi";
+import { redirect } from "react-router-dom";
+
+const buttonStyle = {
+  width: "45%",
+  marginRight:"3px",
+  fontSize: "12px",
+  display:"flex",
+  alignItems:"center",
+  justifyContent:"evenly"
+  
+};
+const buttonTextContainer = {
+  fontSize: "14px",
+  textAlign: "center",
+  margin:"0 10px"
+};
 
 const Boxes = ({ images }) => {
   const boxes = images.map((image) => {
@@ -9,11 +28,14 @@ const Boxes = ({ images }) => {
         height={250}
         style={{
           border: "1px solid black",
-          margin: "20px 20px 20px 20px",
-          padding: "20px 20px 20px 20px",
-          width: "150px",  
-          height: "150px", 
-          borderRadius:"10px"
+          margin: "20px auto",
+          padding: "20px",
+          width: "250px",
+          height: "350px", 
+          borderRadius: "10px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
         <img src={image.src}
@@ -22,8 +44,17 @@ const Boxes = ({ images }) => {
 
          />
         <p>esp-32 kartı</p>
-        <button >Satın Al</button>
-        <button>Sepete Ekle</button>
+        <div style={{ width:"100%", display:"flex"}}>
+          <button style={buttonStyle}  >
+           <span style={buttonTextContainer}>Satın Al</span> 
+           <GiBuyCard />
+            </button>
+        <button style={buttonStyle} >
+         <span style={buttonTextContainer}>Sepete Ekle</span> 
+         <BiCartAdd size={40}/>
+          </button>
+        </div>
+        
       </div>
     );
     return box;
@@ -34,16 +65,37 @@ const Boxes = ({ images }) => {
 const SatınAl = () => {
   const images = [
     {
-        src: kartResim,
+        src: "/images/kart-resim.jpg",
         alt: "esp32 kartı",
-        
+       
     },
+    {
+      src: "/images/kart-resim1.jpg",
+      alt: "esp32 kartı",
+    },
+    {
+      src: "/images/kart-resim1.jpg",
+      alt: "esp32 kartı",
+    },
+    {
+      src: "/images/kart-resim1.jpg",
+      alt: "esp32 kartı",
+    },
+    {
+      src: "/images/kart-resim.jpg",
+      alt: "esp32 kartı",
+     
+  },
    
   ];
 
   return (
     <div>
+      <Navbar/>
+      <div className="parent">
       <Boxes images={images} />
+      </div>
+      
     </div>
   );
 };
